6429be52dedf9cbc0744a9e6f0d316634746f212
Rene G <renenoe94@hotmail.com>
2019-03-08 11:28:49 -0500

    first commit

---
+++ src/main/java/com/wcciproject/masteryblog/MasteryBlogApplication.java
@@ -0,0 +1,13 @@
+package com.wcciproject.masteryblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasteryBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MasteryBlogApplication.class, args);
+	}
+
+}
