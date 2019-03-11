fbf79baa28b8dff628643775ef5ec65d023ddd1d
Eric Hood <hoode1212@gmail.com>
2019-03-10 20:37:58 -0400

    working on time and date

--- src/main/java/com/wcciproject/masteryblog/controllers/GenreController.java
+++ src/main/java/com/wcciproject/masteryblog/controllers/GenreController.java
@@ -24,4 +24,6 @@ public class GenreController {
 	@Resource
 	TagRepository tagRepo;
 	
+	
+	
 }
--- src/main/java/com/wcciproject/masteryblog/models/Post.java
+++ src/main/java/com/wcciproject/masteryblog/models/Post.java
@@ -1,7 +1,6 @@
 package com.wcciproject.masteryblog.models;
 
 import java.util.Collection;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
